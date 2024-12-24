15e3853d179a2771dc15955212f5f40cf29528df
Frédéric Menou <frederic.menou@gmail.com>
2024-12-24 19:39:41 +0100

    Better check script

--- pkgs/check-screenshots/script.sh
+++ pkgs/check-screenshots/script.sh
@@ -15,12 +15,17 @@ function clearLine {
   echo -en "\r\033[K"
 }
 
+function valid {
+  clearLine
+  echo "✅ $1"
+}
+
 function checkScreenshot {
-  echo -n "$1 "
+  echo -n "⌛ $1"
   path=$(readPath "$1")
   if [ -n "$path" ]
   then
-    http --quiet --check-status --headers GET "${baseUrl}${path}" && echo "✅"
+    http --quiet --check-status --headers GET "${baseUrl}${path}" && valid "$1"
   else
     clearLine
   fi
